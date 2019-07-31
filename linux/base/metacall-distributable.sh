711c80af1c5de9d545a08659fc2a8339e6a43d1c
viferga <vic798@gmail.com>
2019-07-31 07:38:44 +0100

    Improve distributable command.

--- linux/base/metacall-distributable.sh
+++ linux/base/metacall-distributable.sh
@@ -24,66 +24,43 @@ help() {
 	echo
 	echo "Usage:"
 	echo
-	echo "LIBS=\"...\" `basename $0` source destination pattern"
+	echo "RESOURCES=\"...\" `basename $0` resource-type source destination pattern"
+	echo "  resource-type:"
+	echo "    lib: copy libraries"
+	echo "    pkg: copy pkgconfig for building with the library"
 	echo "  source: folder where libs are located"
 	echo "  destination: folder where libs are going to be copied"
-	echo "  pattern: special pattern if any, use \${lib} to refer to the current lib being iterated (it must be wrapped by simple commas)"
+	echo "  pattern: special pattern if any, use \${lib} or \${pkg} to refer to the current resource being iterated (it must be wrapped by simple commas)"
 	echo
 	echo "Example:"
 	echo
-	echo "  LIBS=\"ssl sqlite3\" `basename $0` /usr/lib/x86_64-linux-gnu /home/root/portable_libs 'lib\${lib}\\.so.*'"
+	echo "  RESOURCES=\"ssl sqlite3\" `basename $0` lib /usr/lib/x86_64-linux-gnu /home/root/lib 'lib\${lib}\\.so.*'"
+	echo "  RESOURCES=\"zlib\" `basename $0` pkg /usr/lib/x86_64-linux-gnu/pkgconfig /home/root/pkgconfig '${pkg}.pc'"
 	echo
 }
 
-main() {
-# Check if libs are defined
-	if [ -z ${LIBS+x} ]; then
-		echo "Error: Variable LIBS is unset, it must be declared in order to know what libs are going to be copied."
-		help
-		exit 1
-	fi
-
-	# Check if source folder is defined
-	if [ -z ${1+x} ]; then
-		echo "Error: Source folder is not defined."
-		help
-		exit 1
-	fi
-
-	# Check if source folder exists
-	if [ ! -d "${1}" ]; then
-		echo "Error: Source folder does not exist."
-		help
-		exit 1
-	fi
-
-	# Check if destination folder is defined
-	if [ -z ${2+x} ]; then
-		echo "Error: Destination folder is not defined."
-		help
-		exit 1
-	fi
-
-	# Check if pattern is defined
-	if [ -z ${3+x} ]; then
-		echo "Error: Library name pattern is not defined."
-		help
-		exit 1
-	fi
-
+lib() {
 	# Define variable alias
-	source=${1}
-	dest=${2}
-	pattern=${3}
+	type=${1}
+	source=${2}
+	dest=${3}
+	pattern=${4}
 
 	# Create destination folder if it is not created
 	mkdir -p ${dest}
 
 	# Iterate through lib list
-	for lib in ${LIBS}; do
+	for lib in ${RESOURCES}; do
 		alias=`eval echo "${pattern}"`
-		name=$(ls -la ${source} | grep -E "${alias}" | awk '{ print $9 }' | tr ' ' '\n')
-		target=$(ls -la ${source} | grep -E "${alias}" | awk '{ print $11 }' | tr ' ' '\n')
+		name=$(ls -la ${source} | grep -E "${alias}" | awk '{ print $9 }' | tr '\n' ' ' | awk '{ print $NF }' | xargs)
+		target=$(ls -la ${source} | grep -E "${alias}" | awk '{ print $11 }' | tr '\n' ' ' | awk '{ print $NF }' | xargs)
+
+		# Filter properly the name
+		if [ "${target}" = "" ]; then
+			target="${name}"
+		else
+			target=$(basename "${target}")
+		fi
 
 		# Copy library
 		cp ${source}/${target} ${dest}/${target}
@@ -96,6 +73,86 @@ main() {
 		# Show new locations
 		echo "${name} -> ${target}"
 	done
+}
+
+pkg() {
+	# Define variable alias
+	type=${1}
+	source=${2}
+	dest=${3}
+	pattern=${4}
+
+	# Create destination folder if it is not created
+	mkdir -p ${dest}/pkgconfig
+
+	# Iterate through lib list
+	for pkg in ${RESOURCES}; do
+		alias=`eval echo "${pattern}"`
+		pkg="${source}/${alias}"
+		#sed "s#^libdir=.*#libdir=${dest}/lib#g" ${pkg} > ${dest}/pkgconfig/${alias}; \
+
+		# Show new locations
+		echo "origin -------------------------------- ${dest}/lib"
+		echo "${pkg} -------------------------------- ${dest}/pkgconfig/${alias}"
+
+	done
+}
+
+main() {
+	# Check if resources are defined
+	if [ -z ${RESOURCES+x} ]; then
+		echo "Error: Variable RESOURCES is unset, it must be declared in order to know what resources are going to be copied."
+		help
+		exit 1
+	fi
+
+	# Check if resource type is defined
+	if [ -z ${1+x} ]; then
+		echo "Error: Resource type is not defined."
+		help
+		exit 1
+	fi
+
+	# Check if resource type is defined
+	if [ "${1}" != "lib" ] && [ "${1}" != "pkg" ]; then
+		echo "Error: Invalid resource type, use lib or pkg."
+		help
+		exit 1
+	fi
+
+	# Check if source folder is defined
+	if [ -z ${2+x} ]; then
+		echo "Error: Source folder is not defined."
+		help
+		exit 1
+	fi
+
+	# Check if source folder exists
+	if [ ! -d "${2}" ]; then
+		echo "Error: Source folder does not exist."
+		help
+		exit 1
+	fi
+
+	# Check if destination folder is defined
+	if [ -z ${3+x} ]; then
+		echo "Error: Destination folder is not defined."
+		help
+		exit 1
+	fi
+
+	# Check if pattern is defined
+	if [ -z ${4+x} ]; then
+		echo "Error: Library name pattern is not defined."
+		help
+		exit 1
+	fi
+
+	if [ "${1}" = "lib" ]; then
+		lib "$@"
+	elif [ "${1}" = "pkg" ]; then
+		pkg "$@"
+	fi
 }
 
 main "$@"
