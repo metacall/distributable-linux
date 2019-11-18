952893747620e3a58fafe0fce3b65b5f11fd4607
viferga <vic798@gmail.com>
2019-11-18 08:15:50 -0900

    Remove dead code.

--- source/metacall.scm
+++ source/metacall.scm
@@ -16,13 +16,14 @@
 ;	See the License for the specific language governing permissions and
 ;	limitations under the License.
 ;
-
-(use-modules (guix packages)
-      (guix download)
-      (guix build-system cmake)
-      (guix licenses))
-
 (define-public metacall
+  (use-modules
+    (guix packages)
+    (guix download)
+    (guix build-system cmake)
+    (guix licenses)
+  )
+
   (package
     (name "metacall")
     (version "0.1.12")
@@ -34,9 +35,9 @@
       )
     )
     (build-system cmake-build-system)
-    (inputs
-     `(("zlib" ,zlib))
-    )
+    ; (inputs
+    ;  `(("zlib" ,zlib))
+    ; )
     (home-page "https://metacall.io/")
     (synopsis "Inter-language foreign function interface call library")
     (description "METACALL is a library that allows calling functions,
@@ -46,33 +47,3 @@
     (license asl2.0)
   )
 )
-
-
-; (use-modules (guix packages)
-;        (guix git-download)
-;        (guix build-system cmake)
-;        (guix licenses))
-
-; (define-public metacall
-;   (package
-;     (name "metacall")
-;     (version "0.1.12")
-;     (source (origin
-;               (method git-fetch)
-;               (uri (git-reference
-;                     (url "https://github.com/metacall/core.git")
-;                     (commit (string-append "v" version))))
-;               (file-name (git-file-name name version))
-;               (sha256
-;                (base32
-;                 "08mp4sxzbm65ws70ca2f4brdnymwmlalbi61w44g36fmi0jvx9v0"))))
-;     (build-system cmake-build-system)
-;     (inputs
-;      `(("zlib" ,zlib)))
-;     (home-page "https://metacall.io/")
-;     (synopsis "Inter-language foreign function interface call library")
-;     (description "METACALL is a library that allows calling functions,
-; methods or procedures between programming languages.
-; With METACALL you can transparently execute code from / to any
-; programming language, for example, call Python code from JavaScript code")
-;     (license asl2.0)))
