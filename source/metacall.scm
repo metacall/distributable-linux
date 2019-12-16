90ff751b9ac4763653f57716fd26d7eac727d30a
viferga <vic798@gmail.com>
2019-12-16 09:41:12 -0900

    Add NodeJS to MetaCall.

--- source/metacall.scm
+++ source/metacall.scm
@@ -46,14 +46,184 @@
   #:use-module (gnu packages dbm)
   #:use-module (gnu packages ncurses)
   #:use-module (gnu packages tcl)
+  #:use-module (guix utils)
+
+  ; NodeJS
 
   ; Swig
   #:use-module (gnu packages swig)
 
-  ; NodeJS
+  ; RapidJSON
   #:use-module (gnu packages web)
-  #:use-module (gnu packages node)
-  #:use-module (guix utils)
+)
+
+; NodeJS
+(define-public node
+  (package
+    (name "node")
+    (version "10.16.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "https://nodejs.org/dist/v" version
+                                  "/node-v" version ".tar.xz"))
+              (sha256
+               (base32
+                "0236jlb1hxhzqjlmmlxipcycrndiq92c8434iyy7zshh3n4pzqqq"))
+              (modules '((guix build utils)))
+              (snippet
+               `(begin
+                  ;; Remove bundled software.
+                  (for-each delete-file-recursively
+                            '("deps/cares"
+                              "deps/http_parser"
+                              "deps/icu-small"
+                              "deps/nghttp2"
+                              "deps/openssl"
+                              "deps/uv"
+                              "deps/zlib"))
+                  (substitute* "Makefile"
+                    ;; Remove references to bundled software.
+                    (("deps/http_parser/http_parser.gyp") "")
+                    (("deps/uv/include/\\*.h") "")
+                    (("deps/uv/uv.gyp") "")
+                    (("deps/zlib/zlib.gyp") ""))
+                  #t))))
+    (build-system gnu-build-system)
+    (arguments
+     ;; TODO: Purge the bundled copies from the source.
+     '(#:configure-flags '("--shared"
+                           "--shared-cares"
+                           "--shared-http-parser"
+                           "--shared-libuv"
+                           "--shared-nghttp2"
+                           "--shared-openssl"
+                           "--shared-zlib"
+                           "--without-snapshot"
+                           "--with-intl=system-icu")
+       ;; Run only the CI tests.  The default test target requires additional
+       ;; add-ons from NPM that are not distributed with the source.
+       #:test-target "test-ci-js"
+       #:tests? #f ; TODO: Enable tests by removing this line
+       #:phases
+       (modify-phases %standard-phases
+         (add-before 'configure 'patch-files
+           (lambda* (#:key inputs #:allow-other-keys)
+             ;; Fix hardcoded /bin/sh references.
+             (substitute* '("lib/child_process.js"
+                            "lib/internal/v8_prof_polyfill.js"
+                            "test/parallel/test-child-process-spawnsync-shell.js"
+                            "test/parallel/test-stdio-closed.js"
+                            "test/sequential/test-child-process-emfile.js")
+               (("'/bin/sh'")
+                (string-append "'" (which "sh") "'")))
+
+             ;; Fix hardcoded /usr/bin/env references.
+             (substitute* '("test/parallel/test-child-process-default-options.js"
+                            "test/parallel/test-child-process-env.js"
+                            "test/parallel/test-child-process-exec-env.js")
+               (("'/usr/bin/env'")
+                (string-append "'" (which "env") "'")))
+
+             ;; FIXME: These tests fail in the build container, but they don't
+             ;; seem to be indicative of real problems in practice.
+             (for-each delete-file
+                       '("test/parallel/test-cluster-master-error.js"
+                         "test/parallel/test-cluster-master-kill.js"
+                         ;; See also <https://github.com/nodejs/node/issues/25903>.
+                         "test/sequential/test-performance.js"))
+
+             ;; This requires a DNS resolver.
+             (delete-file "test/parallel/test-dns.js")
+
+             ;; These tests have an expiry date: they depend on the validity of
+             ;; TLS certificates that are bundled with the source.  We want this
+             ;; package to be reproducible forever, so remove those.
+             ;; TODO: Regenerate certs instead.
+             (for-each delete-file
+                       '("test/parallel/test-tls-passphrase.js"
+                         "test/parallel/test-tls-server-verify.js"))
+             #t))
+         (replace 'configure
+           ;; Node's configure script is actually a python script, so we can't
+           ;; run it with bash.
+           (lambda* (#:key outputs (configure-flags '()) inputs
+                     #:allow-other-keys)
+             (let* ((prefix (assoc-ref outputs "out"))
+                    (flags (cons (string-append "--prefix=" prefix)
+                                 configure-flags)))
+               (format #t "build directory: ~s~%" (getcwd))
+               (format #t "configure flags: ~s~%" flags)
+               ;; Node's configure script expects the CC environment variable to
+               ;; be set.
+               (setenv "CC" (string-append (assoc-ref inputs "gcc") "/bin/gcc"))
+               (apply invoke
+                      (string-append (assoc-ref inputs "python")
+                                     "/bin/python")
+                      "configure" flags))))
+         (add-after 'patch-shebangs 'patch-npm-shebang
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let* ((bindir (string-append (assoc-ref outputs "out")
+                                           "/bin"))
+                    (npm    (string-append bindir "/npm"))
+                    (target (readlink npm)))
+               (with-directory-excursion bindir
+                 (patch-shebang target (list bindir))
+                 #t)))))))
+    (native-inputs
+     `(("python" ,python-2)
+       ("perl" ,perl)
+       ("pkg-config" ,pkg-config)
+       ("procps" ,procps)
+       ("util-linux" ,util-linux)
+       ("which" ,which)))
+    (native-search-paths
+     (list (search-path-specification
+            (variable "NODE_PATH")
+            (files '("lib/node_modules")))))
+    (inputs
+     `(("c-ares" ,c-ares)
+       ("http-parser" ,http-parser)
+       ("icu4c" ,icu4c)
+       ("libuv" ,libuv)
+       ("nghttp2" ,nghttp2 "lib")
+       ("openssl" ,openssl)
+       ("zlib" ,zlib)))
+    (synopsis "Evented I/O for V8 JavaScript")
+    (description "Node.js is a platform built on Chrome's JavaScript runtime
+for easily building fast, scalable network applications.  Node.js uses an
+event-driven, non-blocking I/O model that makes it lightweight and efficient,
+perfect for data-intensive real-time applications that run across distributed
+devices.")
+    (home-page "https://nodejs.org/")
+    (license expat)
+    (properties '((timeout . 3600))))) ; 1 h
+
+; NodeJS Port Dependencies
+(define-public node-addon-api
+  (package
+    (name "node-addon-api")
+    (version "1.7.1")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (string-append "https://github.com/nodejs/node-addon-api/archive/" version ".tar.gz"))
+        (sha256 (base32 "0i3jc5ki4dlq8l2p1wn0rw1695kr47cjx1zlkzj6h4ymzyc0i1dk"))
+      )
+    )
+    (build-system node-build-system)
+    (arguments
+      `(
+        #:phases
+        (modify-phases %standard-phases
+          (delete 'check)
+        )
+      )
+    )
+    (home-page "https://github.com/nodejs/node-addon-api/")
+    (synopsis "Module for using N-API from C++")
+    (description "This module contains a header-only C++ wrapper classes ...")
+    (license license:expat)
+  )
 )
 
 ; Ruby
@@ -78,6 +248,7 @@
     (build-system gnu-build-system)
     (arguments
      `(#:test-target "test"
+       #:tests? #f ; TODO: Enable tests by removing this line
        #:configure-flags
        (list
          "--enable-shared"
@@ -120,34 +291,6 @@ a focus on simplicity and productivity.")
     (home-page "https://www.ruby-lang.org")
     (license license:ruby)))
 
-; NodeJS Port Dependencies
-(define-public node-addon-api
-  (package
-    (name "node-addon-api")
-    (version "1.7.1")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (string-append "https://github.com/nodejs/node-addon-api/archive/" version ".tar.gz"))
-        (sha256 (base32 "0i3jc5ki4dlq8l2p1wn0rw1695kr47cjx1zlkzj6h4ymzyc0i1dk"))
-      )
-    )
-    (build-system node-build-system)
-    (arguments
-      `(
-        #:phases
-        (modify-phases %standard-phases
-          (delete 'check)
-        )
-      )
-    )
-    (home-page "https://github.com/nodejs/node-addon-api/")
-    (synopsis "Module for using N-API from C++")
-    (description "This module contains a header-only C++ wrapper classes ...")
-    (license license:expat)
-  )
-)
-
 ; MetaCall
 (define-public metacall
   (package
@@ -175,27 +318,43 @@ a focus on simplicity and productivity.")
         #:tests? #f
         #:configure-flags
         (list
+          ; Build wiht release mode
           "-DCMAKE_BUILD_TYPE=Release"
+
           ; Disable stack-smashing protection and source fortify in order to improve libc portability / compatibility
           "-DOPTION_BUILD_SECURITY=OFF"
+
+          ; Enable distributable libs
           "-DOPTION_BUILD_DIST_LIBS=ON"
+
           ; TODO: Enable fork safety
           "-DOPTION_FORK_SAFE=OFF"
           ; TODO: Enable examples
           "-DOPTION_BUILD_EXAMPLES=OFF"
           ; TODO: Enable tests
           "-DOPTION_BUILD_TESTS=OFF"
+
+          ; TODO: Enable when tests
+          "-DOPTION_BUILD_SCRIPTS=OFF"
+          "-DOPTION_BUILD_SCRIPTS_PY=OFF"
+          "-DOPTION_BUILD_SCRIPTS_RB=OFF"
+          "-DOPTION_BUILD_SCRIPTS_FILE=OFF"
+          "-DOPTION_BUILD_SCRIPTS_NODE=OFF"
+          "-DOPTION_BUILD_SCRIPTS_CS=OFF"
+          "-DOPTION_BUILD_LOADERS_JS=OFF"
+
+          ; Serials
           "-DOPTION_BUILD_SERIALS=ON"
           "-DOPTION_BUILD_SERIALS_RAPID_JSON=ON"
           "-DOPTION_BUILD_SERIALS_METACALL=ON"
+
+          ; Loaders
           "-DOPTION_BUILD_LOADERS=ON"
-          ; TODO: Enable when tests
-          "-DOPTION_BUILD_SCRIPTS=OFF"
           "-DOPTION_BUILD_LOADERS_MOCK=ON"
           "-DOPTION_BUILD_LOADERS_PY=ON"
-          "-DOPTION_BUILD_SCRIPTS_PY=OFF" ; TODO: Enable when tests
           "-DOPTION_BUILD_LOADERS_RB=ON"
-          "-DOPTION_BUILD_SCRIPTS_RB=OFF" ; TODO: Enable when tests
+          "-DOPTION_BUILD_LOADERS_FILE=ON"
+          "-DOPTION_BUILD_LOADERS_NODE=ON"
 
           ; TODO: Avoid harcoded versions of Ruby
           (string-append "-DRUBY_EXECUTABLE=" (assoc-ref %build-inputs "ruby") "/bin/ruby")
@@ -203,21 +362,24 @@ a focus on simplicity and productivity.")
           (string-append "-DRUBY_LIBRARY=" (assoc-ref %build-inputs "ruby") "/lib/libruby.so")
           (string-append "-DRUBY_VERSION=2.3.8")
 
+          ; TODO: Avoid harcoded versions of NodeJS
+          (string-append "-DNODEJS_EXECUTABLE=" (assoc-ref %build-inputs "node") "/bin/node")
+          (string-append "-DNODEJS_INCLUDE_DIR=" (assoc-ref %build-inputs "node") "/include/node")
+          (string-append "-DNODEJS_LIBRARY=" (assoc-ref %build-inputs "node") "/lib/libnode.so")
+
           ; `# TODO: -DDOTNET_CORE_PATH=${METACALL_PATH}/netcore/share/dotnet/shared/Microsoft.NETCore.App/${METACALL_NETCORE_VERSION}/` \
 
-          ; TODO: Remove this and enable loaders (and tests + scripts)
+          ; TODO: Remove this and enable loaders
           "-DOPTION_BUILD_LOADERS_CS=OFF"
-          "-DOPTION_BUILD_SCRIPTS_CS=OFF"
-          "-DOPTION_BUILD_LOADERS_JS=OFF"
           "-DOPTION_BUILD_SCRIPTS_JS=OFF"
-          "-DOPTION_BUILD_LOADERS_NODE=OFF"
-          "-DOPTION_BUILD_SCRIPTS_NODE=OFF"
-          "-DOPTION_BUILD_LOADERS_FILE=ON"
-          "-DOPTION_BUILD_SCRIPTS_FILE=OFF"
+
+          ; Ports
           "-DOPTION_BUILD_PORTS=ON"
-          "-DOPTION_BUILD_PORTS_NODE=ON"
           "-DOPTION_BUILD_PORTS_PY=ON"
           "-DOPTION_BUILD_PORTS_RB=ON"
+          "-DOPTION_BUILD_PORTS_NODE=ON"
+
+          ; Disable coverage
           "-DOPTION_COVERAGE=OFF"
 
           ; Python Port (Swig) requires conversion between constant to non-constant char pointer
@@ -228,8 +390,10 @@ a focus on simplicity and productivity.")
     (propagated-inputs
      `(
         ("rapidjson" ,rapidjson)
+        ; TODO: Package only the runtime lib for each runtime
         ("python" ,python)
         ("ruby" ,ruby)
+        ("node" ,node)
       )
     )
     (native-inputs
@@ -244,9 +408,9 @@ a focus on simplicity and productivity.")
     (home-page "https://metacall.io/")
     (synopsis "Inter-language foreign function interface call library")
     (description "METACALL is a library that allows calling functions,
-  methods or procedures between programming languages.
-  With METACALL you can transparently execute code from / to any
-  programming language, for example, call Python code from JavaScript code")
+methods or procedures between programming languages.
+With METACALL you can transparently execute code from / to any
+programming language, for example, call Python code from JavaScript code")
     (license license:asl2.0)
   )
 )
