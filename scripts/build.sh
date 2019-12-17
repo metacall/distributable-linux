d487315f140c9e641e3fa9243188a8c637eb2d54
viferga <vic798@gmail.com>
2019-12-17 03:32:25 -0900

    Pack node alone temporally.

--- scripts/build.sh
+++ scripts/build.sh
@@ -28,6 +28,6 @@ export GUILE_WARN_DEPRECATED='detailed'
 `# Build` guix build node `# ruby node-addon-api metacall` -L /metacall/source \
 `# Test` `# && guix package -i metacall -L /metacall/source` \
 `# Lint` `# && guix lint metacall` \
-`# Pack` && guix pack -RR metacall -L /metacall/source | tee build.log \
+`# Pack` && guix pack -RR `# metacall` node -L /metacall/source | tee build.log \
 `# Copy` && mv `cat build.log | grep "tarball-pack.tar.gz"` /metacall/pack/tarball.tar.gz \
 `# Exit` && exit 0 || exit 1
