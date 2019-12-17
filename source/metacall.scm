8c84a82b237e43519442e91115f97d02d3901477
viferga <vic798@gmail.com>
2019-12-17 03:11:33 -0900

    Testing node alone.

--- source/metacall.scm
+++ source/metacall.scm
@@ -49,7 +49,7 @@
   #:use-module (guix utils)
 
   ; NodeJS
-  #:use-module (gnu packages node)
+  ; #:use-module (gnu packages node)
   #:use-module (gnu packages base)
   #:use-module (gnu packages xml)
   #:use-module (gnu packages adns)
@@ -67,9 +67,9 @@
 )
 
 ; NodeJS
-(define-public libnode
+(define-public node
   (package
-    (name "libnode")
+    (name "node")
     (version "10.16.0")
     (source (origin
               (method url-fetch)
@@ -207,219 +207,219 @@ devices.")
     (license expat)
     (properties '((timeout . 3600))))) ; 1 h
 
-; NodeJS Port Dependencies
-(define-public node-addon-api
-  (package
-    (name "node-addon-api")
-    (version "1.7.1")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (string-append "https://github.com/nodejs/node-addon-api/archive/" version ".tar.gz"))
-        (sha256 (base32 "0i3jc5ki4dlq8l2p1wn0rw1695kr47cjx1zlkzj6h4ymzyc0i1dk"))
-      )
-    )
-    (build-system node-build-system)
-    (arguments
-      `(
-        #:phases
-        (modify-phases %standard-phases
-          (delete 'check)
-        )
-      )
-    )
-    (home-page "https://github.com/nodejs/node-addon-api/")
-    (synopsis "Module for using N-API from C++")
-    (description "This module contains a header-only C++ wrapper classes ...")
-    (license license:expat)
-  )
-)
+; ; NodeJS Port Dependencies
+; (define-public node-addon-api
+;   (package
+;     (name "node-addon-api")
+;     (version "1.7.1")
+;     (source
+;       (origin
+;         (method url-fetch)
+;         (uri (string-append "https://github.com/nodejs/node-addon-api/archive/" version ".tar.gz"))
+;         (sha256 (base32 "0i3jc5ki4dlq8l2p1wn0rw1695kr47cjx1zlkzj6h4ymzyc0i1dk"))
+;       )
+;     )
+;     (build-system node-build-system)
+;     (arguments
+;       `(
+;         #:phases
+;         (modify-phases %standard-phases
+;           (delete 'check)
+;         )
+;       )
+;     )
+;     (home-page "https://github.com/nodejs/node-addon-api/")
+;     (synopsis "Module for using N-API from C++")
+;     (description "This module contains a header-only C++ wrapper classes ...")
+;     (license license:expat)
+;   )
+; )
 
-; Ruby
-(define-public ruby
-  (package
-    (name "ruby")
-    (version "2.3.8")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (string-append "http://cache.ruby-lang.org/pub/ruby/"
-                           (version-major+minor version)
-                           "/ruby-" version ".tar.xz"))
-       (sha256
-        (base32
-         "1zhxbjff08pvbnxvn58krns6q0p6g4977q6ykfn823gxhifn63wi"))
-       (modules '((guix build utils)))
-       (snippet `(begin
-                   ;; Remove bundled libffi
-                   (delete-file-recursively "ext/fiddle/libffi-3.2.1")
-                   #t))))
-    (build-system gnu-build-system)
-    (arguments
-     `(#:test-target "test"
-       #:tests? #f ; TODO: Enable tests by removing this line
-       #:configure-flags
-       (list
-         "--enable-shared"
-       )
-       #:phases
-       (modify-phases %standard-phases
-         (add-before 'configure 'replace-bin-sh-and-remove-libffi
-           (lambda _
-             (substitute* '("Makefile.in"
-                            "ext/pty/pty.c"
-                            "io.c"
-                            "lib/mkmf.rb"
-                            "process.c"
-                            "test/rubygems/test_gem_ext_configure_builder.rb"
-                            "test/rdoc/test_rdoc_parser.rb"
-                            "test/ruby/test_rubyoptions.rb"
-                            "test/ruby/test_process.rb"
-                            "test/ruby/test_system.rb"
-                            "tool/rbinstall.rb")
-               (("/bin/sh") (which "sh")))
-             #t)))))
-    (inputs
-     `(("readline" ,readline)
-       ("openssl" ,openssl)
-       ("bzip2" ,bzip2)
-       ("libffi" ,libffi)
-       ("gdbm" ,gdbm)
-       ("libyaml" ,libyaml)
-       ("ncurses" ,ncurses)
-       ("tcl" ,tcl)
-       ("tk" ,tk) ; TODO: This still fails, Ruby is not able to locate Tk/Tcl lib
-       ("zlib" ,zlib)))
-    (native-search-paths
-     (list (search-path-specification
-            (variable "GEM_PATH")
-            (files (list (string-append "lib/ruby/vendor_ruby"))))))
-    (synopsis "Programming language interpreter")
-    (description "Ruby is a dynamic object-oriented programming language with
-a focus on simplicity and productivity.")
-    (home-page "https://www.ruby-lang.org")
-    (license license:ruby)))
+; ; Ruby
+; (define-public ruby
+;   (package
+;     (name "ruby")
+;     (version "2.3.8")
+;     (source
+;      (origin
+;        (method url-fetch)
+;        (uri (string-append "http://cache.ruby-lang.org/pub/ruby/"
+;                            (version-major+minor version)
+;                            "/ruby-" version ".tar.xz"))
+;        (sha256
+;         (base32
+;          "1zhxbjff08pvbnxvn58krns6q0p6g4977q6ykfn823gxhifn63wi"))
+;        (modules '((guix build utils)))
+;        (snippet `(begin
+;                    ;; Remove bundled libffi
+;                    (delete-file-recursively "ext/fiddle/libffi-3.2.1")
+;                    #t))))
+;     (build-system gnu-build-system)
+;     (arguments
+;      `(#:test-target "test"
+;        #:tests? #f ; TODO: Enable tests by removing this line
+;        #:configure-flags
+;        (list
+;          "--enable-shared"
+;        )
+;        #:phases
+;        (modify-phases %standard-phases
+;          (add-before 'configure 'replace-bin-sh-and-remove-libffi
+;            (lambda _
+;              (substitute* '("Makefile.in"
+;                             "ext/pty/pty.c"
+;                             "io.c"
+;                             "lib/mkmf.rb"
+;                             "process.c"
+;                             "test/rubygems/test_gem_ext_configure_builder.rb"
+;                             "test/rdoc/test_rdoc_parser.rb"
+;                             "test/ruby/test_rubyoptions.rb"
+;                             "test/ruby/test_process.rb"
+;                             "test/ruby/test_system.rb"
+;                             "tool/rbinstall.rb")
+;                (("/bin/sh") (which "sh")))
+;              #t)))))
+;     (inputs
+;      `(("readline" ,readline)
+;        ("openssl" ,openssl)
+;        ("bzip2" ,bzip2)
+;        ("libffi" ,libffi)
+;        ("gdbm" ,gdbm)
+;        ("libyaml" ,libyaml)
+;        ("ncurses" ,ncurses)
+;        ("tcl" ,tcl)
+;        ("tk" ,tk) ; TODO: This still fails, Ruby is not able to locate Tk/Tcl lib
+;        ("zlib" ,zlib)))
+;     (native-search-paths
+;      (list (search-path-specification
+;             (variable "GEM_PATH")
+;             (files (list (string-append "lib/ruby/vendor_ruby"))))))
+;     (synopsis "Programming language interpreter")
+;     (description "Ruby is a dynamic object-oriented programming language with
+; a focus on simplicity and productivity.")
+;     (home-page "https://www.ruby-lang.org")
+;     (license license:ruby)))
 
-; MetaCall
-(define-public metacall
-  (package
-    (name "metacall")
-    (version "0.1.23")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (string-append "https://github.com/metacall/core/archive/v" version ".tar.gz"))
-        (sha256 (base32 "0rdnhvcrap2m0241hq81ssxcci616cyicl0xpyqaik5hqblm9n79"))
-      )
-    )
-    (build-system cmake-build-system)
-    (arguments
-      `(
-        #:phases
-        ; TODO: This may be hidding a CMake bug with rpath on all ports, so this must be reviewed in the future
-        (modify-phases %standard-phases
-          (add-before 'configure 'runpath-workaround
-            (lambda* (#:key outputs #:allow-other-keys)
-              (let ((out (assoc-ref outputs "out")))
-                (setenv "LDFLAGS" (string-append "-Wl,-rpath=" out "/lib"))
-                #t))))
-        ; TODO: Enable tests
-        #:tests? #f
-        #:configure-flags
-        (list
-          ; Build wiht release mode
-          "-DCMAKE_BUILD_TYPE=Release"
+; ; MetaCall
+; (define-public metacall
+;   (package
+;     (name "metacall")
+;     (version "0.1.23")
+;     (source
+;       (origin
+;         (method url-fetch)
+;         (uri (string-append "https://github.com/metacall/core/archive/v" version ".tar.gz"))
+;         (sha256 (base32 "0rdnhvcrap2m0241hq81ssxcci616cyicl0xpyqaik5hqblm9n79"))
+;       )
+;     )
+;     (build-system cmake-build-system)
+;     (arguments
+;       `(
+;         #:phases
+;         ; TODO: This may be hidding a CMake bug with rpath on all ports, so this must be reviewed in the future
+;         (modify-phases %standard-phases
+;           (add-before 'configure 'runpath-workaround
+;             (lambda* (#:key outputs #:allow-other-keys)
+;               (let ((out (assoc-ref outputs "out")))
+;                 (setenv "LDFLAGS" (string-append "-Wl,-rpath=" out "/lib"))
+;                 #t))))
+;         ; TODO: Enable tests
+;         #:tests? #f
+;         #:configure-flags
+;         (list
+;           ; Build wiht release mode
+;           "-DCMAKE_BUILD_TYPE=Release"
 
-          ; Disable stack-smashing protection and source fortify in order to improve libc portability / compatibility
-          "-DOPTION_BUILD_SECURITY=OFF"
+;           ; Disable stack-smashing protection and source fortify in order to improve libc portability / compatibility
+;           "-DOPTION_BUILD_SECURITY=OFF"
 
-          ; Enable distributable libs
-          "-DOPTION_BUILD_DIST_LIBS=ON"
+;           ; Enable distributable libs
+;           "-DOPTION_BUILD_DIST_LIBS=ON"
 
-          ; TODO: Enable fork safety
-          "-DOPTION_FORK_SAFE=OFF"
-          ; TODO: Enable examples
-          "-DOPTION_BUILD_EXAMPLES=OFF"
-          ; TODO: Enable tests
-          "-DOPTION_BUILD_TESTS=OFF"
+;           ; TODO: Enable fork safety
+;           "-DOPTION_FORK_SAFE=OFF"
+;           ; TODO: Enable examples
+;           "-DOPTION_BUILD_EXAMPLES=OFF"
+;           ; TODO: Enable tests
+;           "-DOPTION_BUILD_TESTS=OFF"
 
-          ; TODO: Enable when tests
-          "-DOPTION_BUILD_SCRIPTS=OFF"
-          "-DOPTION_BUILD_SCRIPTS_PY=OFF"
-          "-DOPTION_BUILD_SCRIPTS_RB=OFF"
-          "-DOPTION_BUILD_SCRIPTS_FILE=OFF"
-          "-DOPTION_BUILD_SCRIPTS_NODE=OFF"
-          "-DOPTION_BUILD_SCRIPTS_CS=OFF"
-          "-DOPTION_BUILD_LOADERS_JS=OFF"
+;           ; TODO: Enable when tests
+;           "-DOPTION_BUILD_SCRIPTS=OFF"
+;           "-DOPTION_BUILD_SCRIPTS_PY=OFF"
+;           "-DOPTION_BUILD_SCRIPTS_RB=OFF"
+;           "-DOPTION_BUILD_SCRIPTS_FILE=OFF"
+;           "-DOPTION_BUILD_SCRIPTS_NODE=OFF"
+;           "-DOPTION_BUILD_SCRIPTS_CS=OFF"
+;           "-DOPTION_BUILD_LOADERS_JS=OFF"
 
-          ; Serials
-          "-DOPTION_BUILD_SERIALS=ON"
-          "-DOPTION_BUILD_SERIALS_RAPID_JSON=ON"
-          "-DOPTION_BUILD_SERIALS_METACALL=ON"
+;           ; Serials
+;           "-DOPTION_BUILD_SERIALS=ON"
+;           "-DOPTION_BUILD_SERIALS_RAPID_JSON=ON"
+;           "-DOPTION_BUILD_SERIALS_METACALL=ON"
 
-          ; Loaders
-          "-DOPTION_BUILD_LOADERS=ON"
-          "-DOPTION_BUILD_LOADERS_MOCK=ON"
-          "-DOPTION_BUILD_LOADERS_PY=ON"
-          "-DOPTION_BUILD_LOADERS_RB=ON"
-          "-DOPTION_BUILD_LOADERS_FILE=ON"
-          "-DOPTION_BUILD_LOADERS_NODE=ON"
+;           ; Loaders
+;           "-DOPTION_BUILD_LOADERS=ON"
+;           "-DOPTION_BUILD_LOADERS_MOCK=ON"
+;           "-DOPTION_BUILD_LOADERS_PY=ON"
+;           "-DOPTION_BUILD_LOADERS_RB=ON"
+;           "-DOPTION_BUILD_LOADERS_FILE=ON"
+;           "-DOPTION_BUILD_LOADERS_NODE=ON"
 
-          ; TODO: Avoid harcoded versions of Ruby
-          (string-append "-DRUBY_EXECUTABLE=" (assoc-ref %build-inputs "ruby") "/bin/ruby")
-          (string-append "-DRUBY_INCLUDE_DIR=" (assoc-ref %build-inputs "ruby") "/include/ruby-2.3.0")
-          (string-append "-DRUBY_LIBRARY=" (assoc-ref %build-inputs "ruby") "/lib/libruby.so")
-          (string-append "-DRUBY_VERSION=2.3.8")
+;           ; TODO: Avoid harcoded versions of Ruby
+;           (string-append "-DRUBY_EXECUTABLE=" (assoc-ref %build-inputs "ruby") "/bin/ruby")
+;           (string-append "-DRUBY_INCLUDE_DIR=" (assoc-ref %build-inputs "ruby") "/include/ruby-2.3.0")
+;           (string-append "-DRUBY_LIBRARY=" (assoc-ref %build-inputs "ruby") "/lib/libruby.so")
+;           (string-append "-DRUBY_VERSION=2.3.8")
 
-          ; TODO: Avoid harcoded versions of NodeJS
-          (string-append "-DNODEJS_EXECUTABLE=" (assoc-ref %build-inputs "libnode") "/bin/node")
-          (string-append "-DNODEJS_INCLUDE_DIR=" (assoc-ref %build-inputs "libnode") "/include/node")
-          (string-append "-DNODEJS_LIBRARY=" (assoc-ref %build-inputs "libnode") "/lib/libnode.so")
+;           ; TODO: Avoid harcoded versions of NodeJS
+;           (string-append "-DNODEJS_EXECUTABLE=" (assoc-ref %build-inputs "libnode") "/bin/node")
+;           (string-append "-DNODEJS_INCLUDE_DIR=" (assoc-ref %build-inputs "libnode") "/include/node")
+;           (string-append "-DNODEJS_LIBRARY=" (assoc-ref %build-inputs "libnode") "/lib/libnode.so")
 
-          ; `# TODO: -DDOTNET_CORE_PATH=${METACALL_PATH}/netcore/share/dotnet/shared/Microsoft.NETCore.App/${METACALL_NETCORE_VERSION}/` \
+;           ; `# TODO: -DDOTNET_CORE_PATH=${METACALL_PATH}/netcore/share/dotnet/shared/Microsoft.NETCore.App/${METACALL_NETCORE_VERSION}/` \
 
-          ; TODO: Remove this and enable loaders
-          "-DOPTION_BUILD_LOADERS_CS=OFF"
-          "-DOPTION_BUILD_SCRIPTS_JS=OFF"
+;           ; TODO: Remove this and enable loaders
+;           "-DOPTION_BUILD_LOADERS_CS=OFF"
+;           "-DOPTION_BUILD_SCRIPTS_JS=OFF"
 
-          ; Ports
-          "-DOPTION_BUILD_PORTS=ON"
-          "-DOPTION_BUILD_PORTS_PY=ON"
-          "-DOPTION_BUILD_PORTS_RB=ON"
-          "-DOPTION_BUILD_PORTS_NODE=ON"
+;           ; Ports
+;           "-DOPTION_BUILD_PORTS=ON"
+;           "-DOPTION_BUILD_PORTS_PY=ON"
+;           "-DOPTION_BUILD_PORTS_RB=ON"
+;           "-DOPTION_BUILD_PORTS_NODE=ON"
 
-          ; Disable coverage
-          "-DOPTION_COVERAGE=OFF"
+;           ; Disable coverage
+;           "-DOPTION_COVERAGE=OFF"
 
-          ; Python Port (Swig) requires conversion between constant to non-constant char pointer
-          "-DCMAKE_CXX_FLAGS=-fpermissive"
-        )
-      )
-    )
-    (propagated-inputs
-     `(
-        ("rapidjson" ,rapidjson)
-        ; TODO: Package only the runtime lib for each runtime
-        ("python" ,python)
-        ("ruby" ,ruby)
-        ("libnode" ,libnode)
-      )
-    )
-    (native-inputs
-     `(
-        ("python" ,python)
-        ("ruby" ,ruby)
-        ("node" ,node)
-        ("node-addon-api" ,node-addon-api)
-        ("swig" ,swig)
-      )
-    )
-    (home-page "https://metacall.io/")
-    (synopsis "Inter-language foreign function interface call library")
-    (description "METACALL is a library that allows calling functions,
-methods or procedures between programming languages.
-With METACALL you can transparently execute code from / to any
-programming language, for example, call Python code from JavaScript code")
-    (license license:asl2.0)
-  )
-)
+;           ; Python Port (Swig) requires conversion between constant to non-constant char pointer
+;           "-DCMAKE_CXX_FLAGS=-fpermissive"
+;         )
+;       )
+;     )
+;     (propagated-inputs
+;      `(
+;         ("rapidjson" ,rapidjson)
+;         ; TODO: Package only the runtime lib for each runtime
+;         ("python" ,python)
+;         ("ruby" ,ruby)
+;         ("libnode" ,libnode)
+;       )
+;     )
+;     (native-inputs
+;      `(
+;         ("python" ,python)
+;         ("ruby" ,ruby)
+;         ("node" ,node)
+;         ("node-addon-api" ,node-addon-api)
+;         ("swig" ,swig)
+;       )
+;     )
+;     (home-page "https://metacall.io/")
+;     (synopsis "Inter-language foreign function interface call library")
+;     (description "METACALL is a library that allows calling functions,
+; methods or procedures between programming languages.
+; With METACALL you can transparently execute code from / to any
+; programming language, for example, call Python code from JavaScript code")
+;     (license license:asl2.0)
+;   )
+; )
