b24932947b1eabfa42a51427165953d3949f3254
Hadi Azami <itshaadi@pm.me>
2020-10-03 01:03:10 +0330

    Feat automatic release make sure to set GH_TOKEN and GH_REPO in gitlab CI/CD settings submenu

--- tools/release.sh
+++ tools/release.sh
@@ -19,4 +19,24 @@
 #	limitations under the License.
 #
 
-# TODO
+tag_url="https://api.github.com/repos/${GH_REPO}/releases/tags/${CI_COMMIT_TAG}"
+release_url="https://api.github.com/repos/${GH_REPO}/releases"
+token="Authorization: token ${GH_TOKEN}"
+
+# Create release
+# CI_COMMIT_TITLE as release name
+# CI_COMMIT_DESCRIPTION as release body
+payload=$(printf '{"tag_name": "%s","name": "%s","body": "%s","draft": %s,"prerelease": %s}' "${CI_COMMIT_TAG}" "${CI_COMMIT_TITLE}" "${CI_COMMIT_DESCRIPTION}" "false" "false" )
+api_response=$(curl -s -d "${payload}" -H "${token}" ${release_url})
+echo "${api_response}"
+
+# Get tag info
+tag_info=$(curl -sH ${token} ${tag_url})
+# Get release ID
+eval $(echo "${tag_info}" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+[ "${id}" ] || { echo "Error: Failed to get release id for tag: ${tag}"; echo "${tag_info}" | awk 'length($0)<100' >&2; exit 1; }
+# Upload asset
+upload_url="https://uploads.github.com/repos/${GH_REPO}/releases/${id}/assets?name=${ASSET_NAME}"
+upload_response=$(curl -s --data-binary @"${PWD}/out/tarball.tar.gz" -H "${token}" -H "Content-Type: application/octet-stream" ${upload_url})
+echo "${upload_response}"
+
