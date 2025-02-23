c0a693fe35f587144d4c46cf8f11d204b98b1888
Yasindu Dissanayake <yasindu.mr@gmail.com>
2025-02-23 11:44:20 +0530

    Remove Node Test lines in script files

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -43,6 +43,5 @@ guix build --fallback \
     nss-certs \
      `# dotnet codeanalysis-csharp codeanalysis-common codeanalysis-analyzers` \
     espree typescript \
-    --without-tests=node-lts --without-tests=libnode  --without-tests=node-18.19.0 \
     -L /metacall/nonguix -L /metacall/source 
 
